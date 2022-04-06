8c63a31fec14b1d422934b2991bc2f42a12437dd
Ibrahimt Tameme <ibrahimtameme98@gmail.com>
2022-04-06 14:28:48 +0300

    task3

---
+++ Task3/app.js
@@ -0,0 +1,54 @@
+'use strict';
+let stringg = "aabccbaa";
+function palindrome(str)
+{
+    let len = str.length;
+
+  let answer = true;
+
+    if(len%2 != 0)
+    {
+        len--;
+        let firstpart = str.slice(0,len/2);
+        let lastpart = str.slice(len/2 +1 ,  len +1);
+      
+        console.log(str);
+        console.log(firstpart);
+        console.log(lastpart);
+        let last2 = lastpart.length -1;
+
+        for (let i = 0; i < firstpart.length; i++) {
+            if(firstpart[i] != lastpart[last2])
+            {
+                answer = false;
+            }
+
+            last2--;
+            
+            
+        }
+
+    }
+    else
+    {
+        
+let last2 = str.length - 1;
+        for (let i = 0 ; i < len; i++) {
+            if(str[i] != str[last2])
+            {
+                answer = false;
+            }
+
+            last2 --;
+            
+        }
+
+
+
+    }
+    console.log(answer);
+    return answer;
+}
+
+
+document.body.innerHTML = palindrome(stringg);
